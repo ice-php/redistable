a6ff6b9155f3e7cf8759511589335c4ef25e99c9
icePHP <liyong@nenu.edu.cn>
2018-11-13 18:53:27 +0800

    快捷函数 名称 改为 redisTable

--- src/functions.php
+++ src/functions.php
@@ -5,12 +5,12 @@ namespace icePHP;
 
 /**
  * 创建redis 表对象
- * 这是SRedis的一个快捷入口
+ * 这是RedisTable的一个快捷入口
  * @param string $name 表名
  * @param mixed $orderBy 需要排序索引的字段
  * @return RedisTable
  */
-function redis(string $name, $orderBy = []): RedisTable
+function redisTable(string $name, $orderBy = []): RedisTable
 {
     return new RedisTable($name, $orderBy);
 }
